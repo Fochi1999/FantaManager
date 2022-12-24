6aac94fcb77294437e59bb010d5017e462b2af88
Fochi1999 <fochi.edo99@gmail.com>
2022-12-24 18:49:06 +0100

    updates

--- src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/page_controllers/FormationController.java
+++ src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/page_controllers/FormationController.java
@@ -1,5 +1,6 @@
 package it.unipi.dii.ingin.lsmsd.fantamanager.page_controllers;
 
+import it.unipi.dii.ingin.lsmsd.fantamanager.util.util_controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -32,20 +34,31 @@ public class FormationController implements Initializable {
         for (HBox h: formationBoxes){
             h.getChildren().clear();
         }
+        box_por.getChildren().clear();
         MenuItem scelta= (MenuItem) event.getSource();
         String formationString=scelta.getText();
         System.out.println("E' stata scelta la formazione:");
         String[] numbers=formationString.split("-");
+        ChoiceBox choise_por=new ChoiceBox();
+        choise_por.getStyleClass().add("choise_player");
+        box_por.getChildren().add(choise_por);
         for(int i=0;i< numbers.length;i++){
             System.out.println(numbers[i]);
             HBox act= formationBoxes.get(i);
             for(int j = 0; j<Integer.parseInt(numbers[i]);j++){
-                act.getChildren().add(new ChoiceBox<>());
+                ChoiceBox choise_player=new ChoiceBox();
+                choise_player.getStyleClass().add("choise_player");
+                act.getChildren().add(choise_player);
                 //aggiungi a ogni ChoiceBox la lista delle carte presenti nella collection per quel ruolo per i=0 d,1 mid,2 att
             }
             }
         }
     @FXML
+    protected void click_home() throws IOException {
+
+        Stage stage = (Stage)root.getScene().getWindow();
+        util_controller.back_to_home(stage);
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         formationBoxes =new ArrayList<>();
