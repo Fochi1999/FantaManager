1006dfee824c22fa5b28c976c12c007e07aa6bff
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2022-12-27 17:29:43 +0100

    aggiunte funzioni a collection.java

--- src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/collection/collection.java
+++ src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/collection/collection.java
@@ -3,10 +3,8 @@ package it.unipi.dii.ingin.lsmsd.fantamanager.collection;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
+
 import it.unipi.dii.ingin.lsmsd.fantamanager.player_classes.*;
 
 public class collection {
@@ -159,6 +157,76 @@ public class collection {
 
             public static void closePool(){
                 pool.close();
+            }
+
+            public static void delete_player_from_collection(int player_id){
+                    //anche qui dobbiamo usare la variabile globale per l' id dell' utente
+
+                    System.out.println(player_id);
+
+                    int user_id=1;
+
+                    try (Jedis jedis = pool.getResource()) {
+                        jedis.expire("user_id:"+user_id+":player_id:"+player_id+":name",0);
+                        jedis.expire("user_id:"+user_id+":player_id:"+player_id+":quantity",0);
+                        jedis.expire("user_id:"+user_id+":player_id:"+player_id+":team",0);
+                        jedis.expire("user_id:"+user_id+":player_id:"+player_id+":position",0);
+                    }
+            }
+
+            public void change_team_of_player(int player_id){   //funzione che potrebbe servire all' admin per cambiare il team di un certo giocatore su tutto redis (tipo se va via a gennaio)
+                    System.out.println("Which team does he play for?");
+                    Scanner scanner = new Scanner(System.in);
+
+                    String team=scanner.toString();
+
+                    String key_team="user_id:*:player_id:"+player_id+":team";
+                    try (Jedis jedis = pool.getResource()) {
+                        Set<String> set_keys = jedis.keys(key_team);
+                        Iterator<String> it = set_keys.iterator();
+                        while (it.hasNext()) {
+                            String key = it.next();
+
+                            jedis.set(key,team);
+                        }
+                    }
+
+            }
+
+            public void change_position_of_player(int player_id){ //funzione che potrebbe servire all' admin per cambiare il ruolo di un certo giocatore su tutto redis
+                System.out.println("Which position does he play?");
+                Scanner scanner = new Scanner(System.in);
+
+                String position=scanner.toString();
+
+                String key_pos="user_id:*:player_id:"+player_id+":position";
+                try (Jedis jedis = pool.getResource()) {
+                    Set<String> set_keys = jedis.keys(key_pos);
+                    Iterator<String> it = set_keys.iterator();
+                    while (it.hasNext()) {
+                        String key = it.next();
+
+                        jedis.set(key,position);
+                    }
+                }
+
+            }
+
+            public void add_player_to_collection(int player_id, String name,int quantity, String team, String position){
+
+                    int user_id=1; //dovrebbe essere la variabile globale dell' id dell' utente
+
+                    player_collection player=new player_collection(player_id,name,quantity,team,position);  //forse inutile
+
+                    try (Jedis jedis = pool.getResource()) {
+
+                        jedis.set("user_id:"+user_id+":player_id:"+player_id+":name",player.name);
+                        jedis.set("user_id:"+user_id+":player_id:"+player_id+":quantity", String.valueOf(player.quantity));
+                        jedis.set("user_id:"+user_id+":player_id:"+player_id+":team",player.team);
+                        jedis.set("user_id:"+user_id+":player_id:"+player_id+":position",player.position);
+
+
+                    }
             }
 
 
