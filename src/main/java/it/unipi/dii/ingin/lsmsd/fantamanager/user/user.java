496d054b314d646a7695d338c1469868fe84e885
Fochi1999 <fochi.edo99@gmail.com>
2022-12-29 15:45:18 +0100

    Create user.java

---
+++ src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/user/user.java
@@ -0,0 +1,31 @@
+package it.unipi.dii.ingin.lsmsd.fantamanager.user;
+
+import it.unipi.dii.ingin.lsmsd.fantamanager.formation.formation;
+
+public class user {
+    public String username;
+    String password;
+    String region;
+    String _id;
+    formation[] formations;
+    int credits;
+    int collection;
+    int liv_priv;
+    public user(String nick, String hashPass,String region,String _id, int credits,int collection, int liv_priv){
+        username=nick;
+        password=hashPass;
+        this.region=region;
+        this._id=_id;
+        this.credits=credits;
+        this.collection=collection;
+        this.liv_priv=liv_priv;
+        formations=new formation[38];
+    }
+
+    public int get_liv_priv() {
+        return liv_priv;
+    }
+    public String getUsername(){
+        return username;
+    }
+}
