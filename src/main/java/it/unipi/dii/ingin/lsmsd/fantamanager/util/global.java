d300008a32018ecc705deb3a5c7316c17ed4723e
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2022-12-30 12:25:33 +0100

    aggiunte aggregations sullo shop

--- src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/util/global.java
+++ src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/util/global.java
@@ -11,8 +11,10 @@ public class global {
     public static formation saved_formation_local;
     public static formation saved_formation_server=null;
     public static final String MONGO_URI = "mongodb://localhost:27017";
-    public static final String DATABASE_NAME = "mydatabase";
-    public static final String USERS_COLLECTION_NAME = "users";
+    public static final String DATABASE_NAME = "FantaManager";
+    public static final String USERS_COLLECTION_NAME = "Users";
+
+    public static final String TRADES_COLLECTION_NAME = "Trades";
 
 
 }
