1cec29ee0846189db948141f36a1acdc16b37e3d
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2023-01-10 16:36:43 +0100

    Spostato TODO di cui parlavo prima

--- src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/collection/collection.java
+++ src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/collection/collection.java
@@ -128,7 +128,7 @@ public class collection {
                     String value = jedis.get(key);
                     Integer quantity = Integer.parseInt(value);
                     if (quantity > 1) {
-                        jedis.set("user_id:" + global.id_user + ":card_id:" + card_id + ":quantity", String.valueOf(quantity - 1));  //TODO provare funzionamento
+                        jedis.set("user_id:" + global.id_user + ":card_id:" + card_id + ":quantity", String.valueOf(quantity - 1));
                     }
                     else{
                         jedis.expire("user_id:" + global.id_user + ":card_id:" + card_id + ":name", 0);
@@ -152,7 +152,7 @@ public class collection {
                         try (Jedis jedis = pool.getResource()) {
                             String value=jedis.get(key);
                             Integer quantity=Integer.parseInt(value);
-                            jedis.set("user_id:" + retrieve_user + ":card_id:" + card.card_id + ":quantity", String.valueOf(quantity+1));
+                            jedis.set("user_id:" + retrieve_user + ":card_id:" + card.card_id + ":quantity", String.valueOf(quantity+1));  //TODO provare funzionamento
                         }
                     }
                     else {
