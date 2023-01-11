2482b11738f935c37b4f062bbc7b1afa481758bb
Emmanuel <67012829+Plaza99@users.noreply.github.com>
2023-01-11 12:12:11 +0100

    Updated 'Collection' page graphics

--- src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/page_controllers/CollectionController.java
+++ src/main/java/it/unipi/dii/ingin/lsmsd/fantamanager/page_controllers/CollectionController.java
@@ -13,6 +13,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -34,9 +36,11 @@ public class CollectionController implements Initializable {
     @FXML
     private TextField player_selected;
 
-    @FXML
-    private Button delete_button;
-
+    @FXML private Button delete_button;
+    @FXML private Text delete_warning;
+    @FXML private Pane delete_confirm;
+    @FXML private Pane delete_cancel;
+    
     @FXML
     protected void click_home() throws IOException {
 
@@ -90,11 +94,8 @@ public class CollectionController implements Initializable {
         create_table(collection.load_collection(global.id_user));
 
         delete_button.setDisable(true);
-
-        //TableView.TableViewSelectionModel<LineTable> player = table_collection.getSelectionModel();
-
-
-
+        hide_delete_buttons();
+        
         table_collection.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
 
                 //System.out.println(newValue.fieldProperty("Name").getValue());
@@ -108,19 +109,8 @@ public class CollectionController implements Initializable {
 
 
 
-    public void create_table(ArrayList<card_collection> databaseObject)
-    {
-
-        //TableView<LineTable> table = new TableView<>();
-        //List<Map<String, String>> databaseObject = load_collection();
-        System.out.println("creazione tabella");
-
-        //TableView<LineTable> table = new TableView<>();
-        //JSONArray databaseObject = collection.load_collection(1);  //mi serve id globale
-        //ArrayList<player_collection> databaseObject = collection.load_collection(1);
-        //System.out.println(databaseObject);
-
-
+    public void create_table(ArrayList<card_collection> databaseObject){
+      
         String[] key_set={"Position","Team","Name","Quantity"};
         //double[] column_width={0.2,0.2,0.4,0.2};
 
@@ -132,46 +122,38 @@ public class CollectionController implements Initializable {
             table_collection.getColumns().add(col);
 
         }
-        /*for(double width:column_width){
-            col.prefWidthProperty().bind(table_collection.widthProperty().multiply());
-        }*/
+        
         List<LineTable> data = new ArrayList<>();
 
-
-
         for(int i=0; i<databaseObject.size();i++){
-            //JSONObject player=(JSONObject) databaseObject.get(i);
+           
             card_collection player=databaseObject.get(i);
-
-            //System.out.println(player);
-
+         
             Map<String, String> record = new HashMap<>();
-
 
             record.put("Position", player.get_position());
             record.put("Team", player.get_team());
             record.put("Name", player.get_name());
             record.put("Quantity", String.valueOf(player.get_quantity()));
 
-            //values.add(record);
-            LineTable sequence =new LineTable(record);
-            //System.out.println(sequence);
+            LineTable sequence =new LineTable(record);          
             data.add(sequence);
 
         }
 
-        //System.out.println(data);
-
         table_collection.setItems(FXCollections.observableArrayList(data));
-
-        //table_collection.setPrefWidth(500);
-
-        //HBox root = new HBox();
-        //root.addAll(table);
-
-        //Scene scene = new Scene(root, 500, 500);
-
     }
 
+    public void show_delete_buttons() {
+    	delete_warning.setText("Confirm");
+    	delete_cancel.setVisible(true);
+    	delete_confirm.setVisible(true);
+    }
 
+    public void hide_delete_buttons() {
+    	delete_warning.setText("");
+    	delete_cancel.setVisible(false);
+    	delete_confirm.setVisible(false);
+    }
+    
 }
